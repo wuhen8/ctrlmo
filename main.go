33d61a2a915258f57f04411bcace28a98f349ac5
wuhenba <wuhenba@gmail.com>
2024-12-12 02:44:33 +0800

    create

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "github.com/go-vgo/robotgo"
+    "time"
+)
+
+func main() {
+    // 移动鼠标到指定坐标
+    robotgo.MoveMouse(100, 200)
+    
+    // 鼠标移动并点击
+    robotgo.Click()
+    
+    // 平滑移动
+    robotgo.MoveMouseSmooth(300, 400)
+}
