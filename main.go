5ff200a8dc22e06b5a8bf7c775bb9b3a72ffab67
wuhen8 <93480904+wuhen8@users.noreply.github.com>
2024-12-16 00:52:18 +0000

    i

--- main.go
+++ main.go
@@ -6,11 +6,11 @@ import (
 
 func main() {
     // 移动鼠标到指定坐标
-    robotgo.MoveMouse(100, 200)
+    robotgo.Move(100, 200)
     
     // 鼠标移动并点击
     robotgo.Click()
     
     // 平滑移动
-    robotgo.MoveMouseSmooth(300, 400)
+    robotgo.MoveSmooth(300, 400)
 }
